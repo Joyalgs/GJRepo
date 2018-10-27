e3e8eb9e232fe6d9f9f2243b2be595309b27bbe5
Joyalg <joyalgs@gmail.com>
2018-10-26 22:41:03 -0400

    Created Feature modules

---
+++ TestApp/src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CoreRoutingModule } from './core-routing.module';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CoreRoutingModule
+  ],
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent]
+})
+export class CoreModule { }
