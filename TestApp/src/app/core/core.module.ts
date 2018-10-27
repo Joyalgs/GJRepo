507a05e4752a58b8d483465233595bcd515d7371
Joyalg <joyalgs@gmail.com>
2018-10-26 23:38:35 -0400

    Added bootstarp css and components

--- TestApp/src/app/core/core.module.ts
+++ TestApp/src/app/core/core.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoreRoutingModule } from './core-routing.module';
 import { HeaderComponent } from './header/header.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
-    CoreRoutingModule
+    CoreRoutingModule,
+    NgbModule
   ],
   declarations: [HeaderComponent],
   exports: [HeaderComponent]
