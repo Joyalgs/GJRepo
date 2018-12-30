edf3a52e404c7ebd0a4fd46c908fc42f5f55c0d0
Joyalg <joyalgs@gmail.com>
2018-12-30 03:27:39 -0500

    Auto-generated commit

---
+++ out-tsc/src/app/core/models/IError.js
@@ -0,0 +1,9 @@
+export var ErrorType;
+(function (ErrorType) {
+    ErrorType["Offline"] = "Offline";
+    ErrorType["UIError"] = "UIError";
+    ErrorType["APIError"] = "APIError";
+    ErrorType["APIDown"] = "APIDown";
+    ErrorType["Unknown"] = "Unknown";
+})(ErrorType || (ErrorType = {}));
+//# sourceMappingURL=IError.js.map
